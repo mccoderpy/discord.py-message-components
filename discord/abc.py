ac0dd3e25a6564280a4092609ca67a6581b353f3
Mathieu <80390413+mccoderpy@users.noreply.github.com>
2021-07-05 16:12:25 +0200

    Fixed some Issues when creating an Channel
    I am sure that the error was not my fault but discord.py did not want to help me because it was a 'modification'.

--- discord/abc.py
+++ discord/abc.py
@@ -175,7 +175,10 @@ class _Overwrites:
         self.id = kwargs.pop('id')
         self.allow = int(kwargs.pop('allow_new', 0))
         self.deny = int(kwargs.pop('deny_new', 0))
-        self.type = sys.intern(kwargs.pop('type'))
+        type = kwargs.pop('type')
+        if type not in ('role', 'user', 'member'):
+            type = {0: 'role', 1: 'user'}.get(type)
+        self.type = sys.intern(type)
 
     def _asdict(self):
         return {
