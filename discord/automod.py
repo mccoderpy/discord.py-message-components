07d7bcde3f08e85a0bec2000101ae928246b8e04
Mathieu Corsham <McCuber04@outlook.de>
2022-06-19 17:56:26 +0200

    feat(implement:AutoMod): Added properties and finished init of AutoModRule

--- discord/automod.py
+++ discord/automod.py
@@ -40,13 +40,17 @@ from typing import (
 
 import datetime
 
-from .abc import Snowflake
+from .role import Role
+from .abc import Snowflake, GuildChannel
+from .object import Object
 from .enums import AutoModEventType, AutoModKeywordPresetType, AutoModActionType, AutoModTriggerType, try_enum
 
 
 if TYPE_CHECKING:
     # Add imports here, that are only used for annotation and would raise CircularImportError otherwise
-    pass
+    from .state import ConnectionState
+    from .guild import Guild
+    from .member import Member
 
 
 class AutoModAction:
@@ -107,19 +111,183 @@ class AutoModTriggerMetadata:
                 Field is only required if :attr:`AutoModRule.trigger_type` is :attr:`AutoModTriggerType.keyword_preset`
         """
 
-        self.keyword_filter: Optional[List[str]] = keyword_filter
-        self.presets: Optional[List[AutoModKeywordPresetType]] = presets
+        self.keyword_filter: Optional[List[str]] = keyword_filter or []
+        self.presets: Optional[List[AutoModKeywordPresetType]] = presets or []
+
+    @property
+    def prefix_keywords(self):
+        """
+        Returns all keywords for words that must start with the keyword.
+
+        .. note::
+            This is equal to
+            .. code-block:: python3
+
+                for keyword in self.keyword_filter:
+                    if keyword[0] != '*' and keyword[-1] == '*':
+                        yield keyword
+                        
+        Yields
+        -------
+        :class:`str`
+            A keyword
+        """
+        for keyword in self.keyword_filter:
+            if keyword[0] != '*' and keyword[-1] == '*':
+                yield keyword
+
+    @property
+    def suffix_keywords(self):
+        """
+        Returns all keywords for words that must end with the keyword.
+
+        .. note::
+            This is equal to
+            .. code-block:: python3
+
+                for keyword in self.keyword_filter:
+                    if keyword[0] == '*' and keyword[-1] != '*':
+                        yield keyword
+
+        Yields
+        -------
+        :class:`str`
+            A keyword
+        """
+        for keyword in self.keyword_filter:
+            if keyword[0] != '*' and keyword[-1] == '*':
+                yield keyword
+
+    @property
+    def anywhere_keywords(self):
+        """
+        Returns all keywords which can appear anywhere in a word
+
+        .. note::
+            This is equal to
+            .. code-block:: python3
+
+                for keyword in self.keyword_filter:
+                    if keyword[0] == '*' and keyword[-1] == '*':
+                        yield keyword
+
+        Yields
+        -------
+        :class:`str`
+            A keyword
+        """
+        for keyword in self.keyword_filter:
+            if keyword[0] == '*' and keyword[-1] == '*':
+                yield keyword
+
+    @property
+    def whole_word_keywords(self):
+        """
+        Returns all keywords that must occur as a whole in a word or phrase and must be surrounded by spaces.
+
+        .. note::
+            This is equal to
+            .. code-block:: python3
+
+                for keyword in self.keyword_filter:
+                    if keyword[0] != '*' and keyword[-1] != '*':
+                        yield keyword
+
+        Yields
+        -------
+        :class:`str`
+            A keyword
+        """
+        for keyword in self.keyword_filter:
+            if keyword[0] != '*' and keyword[-1] != '*':
+                yield keyword
 
 
 class AutoModRule:
     def __init__(self,
-                 event_type: AutoModEventType,
-                 trigger_type: AutoModTriggerType,
-                 actions: List[AutoModAction],
-                 trigger_metadata: AutoModTriggerMetadata,
-                 enabled: bool,
-                 except_roles: List[Snowflake] = [],
-                 except_channels: List[Snowflake] = [],
+                 state: ConnectionState,
+                 guild: Guild,
+                 data: Dict[str, Any]
                  ):
-        #TODO: Get some sleep now 😅 will continue tomorrow
-        pass
+        """
+        .. warning::
+            Do not initialize this class directly. Use :func:`discord.Guild.add_auto_mod_rule` instead.
+
+        Attributes
+        -----------
+        id: :class:`int`
+            The id of this rule
+        guild: :class:`~discord.Guild`
+            The guild  this rule belongs to
+        name: :class:`str`
+            The name of the rule
+        creator_id: :class:`int`
+            The id of the user wich created this rule
+        event_type: :class:`AutoModEventType`
+            The event wich will trigger this rule
+        trigger_type: :class:`AutoModEventType`
+            The type of content which will trigger the rule
+        trigger_metadata: :class:`AutoModTriggerMetadata`
+            Additional data used to determine whether a rule should be triggered.
+            Different fields are relevant based on the value of :attr:`.trigger_type`.
+        actions: List[:class:AutoModAction`]
+            The actions which will execute when the rule is triggered
+        enabled: :class:`bool`
+            Whether the rule is enabled
+        """
+        self._state: ConnectionState = state
+        self.id: int = int(data['id'])
+        self.guild: Guild = guild
+        self.name: str = data['name']
+        self.creator_id: int = int(data['creator_id'])
+        self.event_type: AutoModEventType = try_enum(AutoModEventType, data['event_type'])
+        self.trigger_type: AutoModTriggerType = try_enum(AutoModTriggerType, data['trigger_type'])
+        self.trigger_metadata: AutoModTriggerMetadata = AutoModTriggerMetadata(**data['trigger_metadata'])
+        self.actions: List[AutoModAction] = [AutoModAction(**action) for action in data['actions']]
+        self.enabled: bool = data['enabled']
+        self._except_roles: List[Snowflake] = [role_id for role_id in data.get('except_roles', [])],
+        self._except_channels: List[Snowflake] = [channel_id for channel_id in data.get('except_channels', [])],
+
+    @property
+    def except_roles(self):
+        """
+        Yields the roles that should not be affected by the rule (Maximum of 20)
+
+        .. note::
+            This is equal to
+            .. code-block:: python3
+
+                for role_id in self._except_roles:
+                    role = self.guild.get_role(int(role_id))
+                    yield role or Object(int(role_id))
+
+        Yields
+        -------
+        Union[:class:`~discord.Role`, :class:`~discord.Object`]
+            An excluded role or an object with the id if the role is not found
+        """
+        for role_id in self._except_roles:
+            role = self.guild.get_role(int(role_id))
+            yield role or Object(role_id, _type=Role, state=self._state)
+
+    @property
+    def except_channels(self):
+        """
+        Yields the channels that should not be affected by the rule (Maximum of 20)
+
+        .. note::
+            This is equal to
+            .. code-block:: python3
+
+                for channel_id in self._except_channels:
+                    channel = self.guild.get_role(int(channel_id))
+                    yield channel or Object(channel_id, _type=GuildChannel, state=self._state)
+
+        Yields
+        -------
+        Union[:class:`~discord.Role`, :class:`~discord.Object`]
+            An excluded channel or an :class:`~discord.object` with the id if the channel is not found
+        """
+        for channel_id in self._except_channels:
+            channel = self.guild.get_role(int(channel_id))
+            yield channel or Object(channel_id, _type=GuildChannel, state=self._state)
