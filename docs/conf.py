bfe097e6b76490aeda1bb57a5a9d3cb709784839
Mathieu Corsham <McCuber04@outlook.de>
2022-06-23 04:44:25 +0200

    feat(docs): Fixing banner........ soon

--- docs/conf.py
+++ docs/conf.py
@@ -152,7 +152,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx-rtd-theme'
+html_theme = 'sphinx_rtd_theme'
 
 # The name of a javascript file (relative to the configuration directory) that
 # implements a search results scorer. If empty, the default will be used.
