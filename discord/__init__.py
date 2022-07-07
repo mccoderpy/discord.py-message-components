1fb408e566c54e63f6bf915eb40fd24127fc972f
Mathieu Corsham <McCuber04@outlook.de>
2022-07-07 20:45:16 +0200

    fix(general): Just delete the fp after __version__ was set

--- discord/__init__.py
+++ discord/__init__.py
@@ -16,9 +16,11 @@ __author__ = 'Rapptz & mccoderpy'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015-2021 Rapptz & 2021-present mccoderpy'
 __version__ = '1.8a'
+
 try:
     with open('./version.txt') as fp:
         __version__ = fp.read()
+    del fp
 except Exception:
     pass
 
