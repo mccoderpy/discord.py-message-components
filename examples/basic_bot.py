2309e4078f4fa0b5bd44161293e8dfcdd39a1fa2
Mathieu Corsham <McCuber04@outlook.de>
2022-08-28 17:55:54 +0200

    feat(examples): Fixed example bot template

--- examples/basic_bot.py
+++ examples/basic_bot.py
@@ -1,9 +1,18 @@
+from pathlib import Path
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('.!'), intents=discord.Intents.all(), case_insensitive=True)
-
 TOKEN = "TOKEN"
+cog_directory = "cogs"  # directory containing the extensions (cogs)
+
+
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned_or('!'),
+    intents=discord.Intents.all(),
+    case_insensitive=True,
+    sync_commands=True
+)
+
 
 @bot.event
 async def on_ready():
@@ -11,8 +20,12 @@ async def on_ready():
     print(f"{bot.user.name}")
     print("======")
 
+
 @bot.command()
 async def ping(ctx):
     ctx.send("Pong!")
 
-bot.run(TOKEN)
+if __name__ == "__main__":
+    _cogs = [p.stem for p in Path(cog_directory).glob('*.py')]
+    [(bot.load_extension(f'.{ext}', package=cog_directory), print(f'{ext} was loaded successfully')) for ext in _cogs]
+    bot.run(TOKEN)
