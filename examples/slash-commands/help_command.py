21df113a7da71f5d8efa2ed13579b31c9ab1f3fd
Mathieu Corsham <McCuber04@outlook.de>
2022-08-28 16:44:17 +0200

    feat(examples): Added example help-command for slash-commands

--- examples/slash-commands/help_command.py
+++ examples/slash-commands/help_command.py
@@ -1,26 +1,68 @@
-import asyncio
-from datetime import datetime
-from typing import Optional, List
+from typing import (
+    Optional,
+    Union
+)
 
 import discord
-from discord import SlashCommandOption as Option, Localizations, Permissions, ApplicationCommandInteraction as APPCI
+from discord import (
+    SlashCommandOption as Option,
+    ApplicationCommandInteraction as APPCI
+)
 from discord.ext import commands
+
 
 class HelpCommand(commands.Cog):
     def __init__(self, bot):
-        self.bot = bot
+        self.bot: commands.Bot = bot
 
-    @commands.Cog.slash_command(name="help", description="Shows you the Help Menu", guild_ids=[977326610334228502])
-    async def help(self, ctx: APPCI):
-        embed = discord.Embed(title=f"Help Menu for {ctx.bot.user}")
-        embed.add_field(name="/<yourcommand>", value="Description 1", inline=True)
-        embed.add_field(name="/<yourcommand2>", value="Description 2", inline=True)
-        embed.add_field(name="/<yourcommand3>", value="Description 3", inline=True)
-        embed.add_field(name="/<yourcommand4>", value="Description 4", inline=True)
-        embed.add_field(name="/<yourcommand5>", value="Description 5", inline=True)
-        await APPCI.respond(embed=embed)
-
-
+    @commands.Cog.slash_command(
+        description='Shows you the help menu',
+        options=[
+            Option(
+                name='command',
+                description='The command to show extra information for',
+                option_type=str,
+                autocomplete=True,
+                required=False
+            )
+        ]
+    )
+    async def help(self, ctx: APPCI, command: Optional[str] = None):
+        if command is None:
+            description = ''
+            for cmd in self.bot.global_application_commands:
+                description += f'{cmd.mention} - {cmd.description}\n'
+            embed = discord.Embed(
+                title=f'Help menu for {self.bot.user.name}',
+                description=description,
+                color=discord.Color.blurple()
+            )
+        else:
+            command: Union[discord.SlashCommand, discord.SubCommand] = discord.utils.get(
+                self.bot.global_application_commands,
+                qualified_name=command
+            )
+            embed = discord.Embed(
+                title=f'Info for command {command.name}',
+                description=f'**Usage of {command.mention}:**\n{command.description}',
+                color=discord.Color.blurple()
+            )
+            if command.options:
+                embed.description += f'\n\n' \
+                                     f'**Usage:**\n' \
+                                     f'`<>` means the option is required, `[]` that it is optional\n' \
+                                     f'**/{command.qualified_name}** '
+                for option in command.options:
+                    if option.required:
+                        embed.description += f'<`{option.name}`> '
+                    else:
+                        embed.description += f'[`{option.name}`] '
+                    embed.add_field(
+                        name=option.name + ' (optional)' if not option.required else '',
+                        value=option.description,
+                        inline=False
+                    )
+        await ctx.respond(embed=embed)
 
 
 def setup(bot):
