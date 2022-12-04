93215c63013fd31582b7010bebecbabc77a02286
Mathieu Corsham <McCuber04@outlook.de>
2022-12-05 00:50:02 +0100

    feat(types): Introducing types package

---
+++ discord/types/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+discord4py.types
+~~~~~~~~~~~~~~~~~~~
+
+Typings for the discord-api models
+
+:copyright: (c) 2021-present mccoderpy
+:license: MIT, see LICENSE for more details.
+"""
