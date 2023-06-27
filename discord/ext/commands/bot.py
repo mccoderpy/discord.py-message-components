mccoderpy/discord.py-message-components

commit 9f16b22f0301a96d1d835c9d1944e50b6c65fbd8

feat: Add `match` attribute to interactions when on_click/select/submit decorator was used
Signed-off-by: mccoderpy <mccuber04@outlook.de>

discord/ext/commands/bot.py
@@ -34,9 +34,11 @@ import sys
 import traceback
 import types
 from typing import (
+    Callable,
     Optional,
     Union,
     Dict,
+    List,
     Tuple
 )
 
@@ -112,8 +114,8 @@ class BotBase(GroupMixin):
     def __init__(self, command_prefix, help_command=_default, description=None, **options):
         super().__init__(**options)
         self.command_prefix = command_prefix
-        self.extra_events = {}
-        self.extra_interaction_events = {}
+        self.extra_events: Dict[str, List[Callable[..., ...]]] = {}
+        self.extra_interaction_events: Dict[str, List[Tuple[Callable[..., ...], Callable[..., ...]]]] = {}
         self.__cogs = {}
         self.__extensions = {}
         self._checks = []
@@ -442,7 +444,7 @@ class BotBase(GroupMixin):
         This adds an interaction(decorator) like :meth:`on_click` or :meth:`on_select` to the client listeners.
 
         .. note::
-            This should not use directly; only cogs use this to register them.
+            This should not be used directly; it's used internal when a Cog is loaded.
 
         """
         try:
@@ -451,24 +453,35 @@ class BotBase(GroupMixin):
             listeners = []
             self.extra_interaction_events[_type] = listeners
         if _type == 'modal_submit':
-            listeners.append((func, lambda i: custom_id.match(str(i.custom_id))))
+            def _check(i):
+                match = custom_id.match(str(i.custom_id))
+                if match:
+                    i.match = match
+                    return True
+                return False
         else:
-            listeners.append((func, lambda i, c: custom_id.match(str(c.custom_id))))
+            def _check(i, b):
+                match = custom_id.match(str(b.custom_id))
+                if match:
+                    i.match = match
+                    return True
+                return False
 
-    def remove_interaction_listener(self, _type, func, custom_id: re.Pattern):
+        # We store this in the function here, so we can remove it later easily
+        listener = (func.__func__, _check)
+        setattr(func.__func__, '__listener__', listener)
+        listeners.append(listener)
+
+    def remove_interaction_listener(self, _type, func):
         """
         This removes an interaction(decorator) like :meth:`on_click` or :meth:`on_select` from the client listeners.
 
         .. note::
-            This should not use directly; only cogs use this to remove them.
-
+            This should not be used directly; it's used internal when a Cog is un-loaded.
         """
         try:
             if _type in self.extra_interaction_events:
-                if _type == 'modal_submit':
-                    self.extra_interaction_events[_type].remove((func, lambda i: custom_id.match(str(i.custom_id))))
-                else:
-                    self.extra_interaction_events[_type].remove((func, lambda i, c: custom_id.match(str(c.custom_id))))
+                self.extra_interaction_events[_type].remove(func.__listener__)
         except ValueError:
             pass
 

.sourcery.yaml
@@ -1,0 +1,3 @@
+rule_settings:
+  disable:
+  - use-named-expression

discord/client.py
@@ -1392,7 +1392,7 @@ class Client:
             You can also specify a regex and if the custom_id matches it, the function will be executed.
 
             .. note::
-                As the :attr:`custom_id` is converted to a `Pattern <https://docs.python.org/3.9/library/re.html#re-objects>`_
+                As the ``custom_id`` is converted to a `Pattern <https://docs.python.org/3/library/re.html#re-objects>`_
                 put ``^`` in front and ``$`` at the end
                 of the :attr:`custom_id` if you want that the custom_id must exactly match the specified value.
                 Otherwise, something like 'cool blue Button is blue' will let the function bee invoked too.
@@ -1434,7 +1434,14 @@ class Client:
                 listeners = []
                 self._listeners['raw_button_click'] = listeners
 
-            listeners.append((func, lambda i, c: _custom_id.match(str(c.custom_id))))
+            def _check(i: ComponentInteraction, c: Button) -> bool:
+                match = _custom_id.match(str(c.custom_id))
+                if match:
+                    i.match = match
+                    return True
+                return False
+
+            listeners.append((func, _check))
             return func
 
         return decorator
@@ -1461,7 +1468,7 @@ class Client:
             You can also specify a regex and if the custom_id matches it, the function will be executed.
 
             .. note::
-                As the :attr:`custom_id` is converted to a `Pattern <https://docs.python.org/3.9/library/re.html#re-objects>`_
+                As the ``custom_id`` is converted to a `Pattern <https://docs.python.org/3/library/re.html#re-objects>`_
                 put ``^`` in front and ``$`` at the end
                 of the :attr:`custom_id` if you want that the custom_id must exactly match the specified value.
                 Otherwise, something like 'choose_your_gender later' will let the function bee invoked too.
@@ -1502,7 +1509,14 @@ class Client:
                 listeners = []
                 self._listeners['raw_selection_select'] = listeners
 
-            listeners.append((func, lambda i, c: _custom_id.match(str(c.custom_id))))
+            def _check(i: ComponentInteraction, c: Button) -> bool:
+                match = _custom_id.match(str(c.custom_id))
+                if match:
+                    i.match = match
+                    return True
+                return False
+
+            listeners.append((func, _check))
             return func
 
         return decorator
@@ -1525,29 +1539,55 @@ class Client:
         Parameters
         ----------
         custom_id: Optional[Union[Pattern[AnyStr], AnyStr]]
-            If the `custom_id` of the :class:`~discord.Modal` could not be used as a function name,
+            If the :attr:`~discord.Modal.custom_id` of the modal could not be used as a function name,
             or you want to give the function a different name then the custom_id use this one to set the custom_id.
             You can also specify a regex and if the custom_id matches it, the function will be executed.
 
             .. note::
-                As the :attr:`custom_id` is converted to a `Pattern <https://docs.python.org/3.9/library/re.html#re-objects>`_
+                As the ``custom_id`` is converted to a
+                `Pattern <https://docs.python.org/3/library/re.html#re-objects>`_
                 put ``^`` in front and ``$`` at the end of the :attr:`custom_id` if you want that the custom_id must
                 exactly match the specified value.
                 Otherwise, something like 'suggestions_modal_submit_private' will let the function bee invoked too.
 
-        Example
-        -------
+            .. tip::
+                The resulting `Match <https://docs.python.org/3/library/re.html#match-objects>`_ object will be
+                available under the :class:`~discord.ModalSubmitInteraction.match` attribute of the interaction.
+
+                **See example below.**
+
+        Examples
+        --------
         .. code-block:: python
+            :caption: Simple example of a Modal with a custom_id and a function that's called when the Modal is submitted.
+            :emphasize-lines: 9, 14
 
             # the Modal
-            Modal(title='Create a new suggestion',
-                  custom_id='suggestions_modal',
-                  components=[...])
+            Modal(
+                title='Create a new suggestion',
+                custom_id='suggestions_modal',
+                components=[...]
+            )
 
             # function that's called when the Modal is submitted
             @client.on_submit(custom_id='^suggestions_modal$')
             async def suggestions_modal_callback(i: discord.ModalSubmitInteraction):
                 ...
+
+            # This can also be done based on the function name
+            @client.on_submit()
+            async def suggestions_modal(i: discord.ModalSubmitInteraction):
+                ...
+
+
+        .. code-block:: python
+            :caption: You can also use a more advanced RegEx containing groups to easily allow dynamic custom-id's
+            :emphasize-lines: 1, 3
+
+            @client.on_submit(custom_id='^ticket_answer:(?P<id>[0-9]+)$')
+            async def ticket_answer_callback(i: discord.ModalSubmitInteraction):
+                user_id = int(i.match['id'])
+                user = client.get_user(user_id) or await client.fetch_user(user_id)
 
         Raises
         ------
@@ -1568,7 +1608,14 @@ class Client:
                 listeners = []
                 self._listeners['modal_submit'] = listeners
 
-            listeners.append((func, lambda i: _custom_id.match(str(i.custom_id))))
+            def _check(i: ModalSubmitInteraction) -> bool:
+                match = _custom_id.match(str(i.custom_id))
+                if match:
+                    i.match = match
+                    return True
+                return False
+
+            listeners.append((func, _check))
             return func
 
         return decorator

discord/ext/commands/cog.py
@@ -115,7 +115,7 @@ class CogMeta(type):
 
         commands = {}
         listeners = {}
-        cog_interaction_listeners = {}
+        interaction_listeners = {}
 
         no_bot_cog = 'Commands or listeners must not start with cog_ or bot_ (in method {0.__name__}.{1})'
 
@@ -148,7 +148,7 @@ class CogMeta(type):
                         else:
                             if elem.startswith(('cog_', 'bot_')):
                                 raise TypeError(no_bot_cog.format(base, elem))
-                            cog_interaction_listeners[elem] = value
+                            interaction_listeners[elem] = value
                     else:
                         if elem.startswith(('cog_', 'bot_')):
                             raise TypeError(no_bot_cog.format(base, elem))
@@ -163,12 +163,13 @@ class CogMeta(type):
                 # the self attribute when the time comes to add them to the bot
                 listeners_as_list.append((listener_name, listener.__name__))
 
-        interaction_listeners_as_list = []
-        for interaction_listener in cog_interaction_listeners.values():
-            for listener_name, custom_id in interaction_listener.__interaction_listener_names__:
-                interaction_listeners_as_list.append((listener_name, interaction_listener.__name__, custom_id))
-
         new_cls.__cog_listeners__ = listeners_as_list
+
+        interaction_listeners_as_list = []
+        for interaction_listener in interaction_listeners.values():
+            for listener_name, pattern in interaction_listener.__interaction_listener_names__:
+                interaction_listeners_as_list.append((listener_name, interaction_listener.__name__, pattern))
+
         new_cls.__cog_interaction_listeners__ = interaction_listeners_as_list
 
         return new_cls
@@ -356,7 +357,7 @@ class Cog(metaclass=CogMeta):
             You can also specify a regex and if the custom_id matches it, the function will be executed.
 
             .. note::
-                As the :attr:`custom_id` is converted to a `Pattern <https://docs.python.org/3.9/library/re.html#re-objects>`_
+                As the ``custom_id`` is converted to a `Pattern <https://docs.python.org/3/library/re.html#re-objects>`_
                 put ``^`` in front and ``$`` at the end
                 of the :attr:`custom_id` if you want that the custom_id must exactly match the specified value.
                 Otherwise, something like 'cool blue Button is blue' will let the function bee invoked too.
@@ -387,13 +388,13 @@ class Cog(metaclass=CogMeta):
             if not inspect.iscoroutinefunction(actual):
                 raise TypeError('event registered must be a coroutine function')
             actual.__cog_interaction_listener__ = True
-            _custom_id = re.compile(custom_id) if (
+            pattern = re.compile(custom_id) if (
                     custom_id is not None and not isinstance(custom_id, re.Pattern)
             ) else re.compile(f'^{actual.__name__}$')
             try:
-                actual.__interaction_listener_names__.append(('raw_button_click', _custom_id))
+                actual.__interaction_listener_names__.append(('raw_button_click', pattern))
             except AttributeError:
-                actual.__interaction_listener_names__ = [('raw_button_click', _custom_id)]
+                actual.__interaction_listener_names__ = [('raw_button_click', pattern)]
             return func
         return decorator
 
@@ -419,7 +420,7 @@ class Cog(metaclass=CogMeta):
             You can also specify a regex and if the custom_id matches it, the function will be executed.
 
             .. note::
-                As the :attr:`custom_id` is converted to a `Pattern <https://docs.python.org/3.9/library/re.html#re-objects>`_
+                As the ``custom_id`` is converted to a `Pattern <https://docs.python.org/3/library/re.html#re-objects>`_
                 put ``^`` in front and ``$`` at the end
                 of the :attr:`custom_id` if you want that the custom_id must exactly match the specified value.
                 Otherwise, something like 'choose_your_gender later' will let the function bee invoked too.
@@ -454,18 +455,18 @@ class Cog(metaclass=CogMeta):
             if not inspect.iscoroutinefunction(actual):
                 raise TypeError('event registered must be a coroutine function')
             actual.__cog_interaction_listener__ = True
-            _custom_id = re.compile(custom_id) if (
+            pattern = re.compile(custom_id) if (
                     custom_id is not None and not isinstance(custom_id, re.Pattern)
             ) else re.compile(f'^{actual.__name__}$')
             try:
-                actual.__interaction_listener_names__.append(('raw_selection_select', _custom_id))
+                actual.__interaction_listener_names__.append(('raw_selection_select', pattern))
             except AttributeError:
-                actual.__interaction_listener_names__ = [('raw_selection_select', _custom_id)]
+                actual.__interaction_listener_names__ = [('raw_selection_select', pattern)]
             return func
         return decorator
 
     @classmethod
-    def on_submit(self, custom_id: Optional[Union[Pattern[AnyStr], AnyStr]] = None) -> Callable[
+    def on_submit(cls, custom_id: Optional[Union[Pattern[AnyStr], AnyStr]] = None) -> Callable[
         [Awaitable[Any]], Awaitable[Any]
     ]:
         """
@@ -481,15 +482,21 @@ class Cog(metaclass=CogMeta):
         Parameters
         ----------
         custom_id: Optional[Union[Pattern[AnyStr], AnyStr]]
-            If the :attr:`custom_id` of the :class:`~discord.Modal` could not use as a function name,
+            If the :attr:`~discord.Modal.custom_id` of the modal could not use as a function name,
             or you want to give the function a different name then the custom_id use this one to set the custom_id.
             You can also specify a regex and if the custom_id matches it, the function will be executed.
 
             .. note::
-                As the :attr:`custom_id` is converted to a `Pattern <https://docs.python.org/3.9/library/re.html#re-objects>`_
+                As the ``custom_id`` is converted to a `Pattern <https://docs.python.org/3/library/re.html#re-objects>`_
                 put ``^`` in front and ``$`` at the end
                 of the :attr:`custom_id` if you want that the custom_id must exactly match the specified value.
                 Otherwise, something like 'suggestions_modal_submit_private' will let the function bee invoked too.
+
+            .. tip::
+                The resulting `Match <https://docs.python.org/3/library/re.html#match-objects>`_ object will be
+                available under the :class:`~discord.ModalSubmitInteraction.match` attribute of the interaction.
+
+                **See example below.**
 
         Example
         -------
@@ -501,9 +508,16 @@ class Cog(metaclass=CogMeta):
                   components=[...])
 
             # function that's called when the Modal is submitted
-            @commands.Cog.on_submit(custom_id='suggestions_modal')
+            @commands.Cog.on_submit(custom_id='^suggestions_modal$')
             async def suggestions_modal_callback(self, i: discord.ModalSubmitInteraction):
                 ...
+
+            # You can also use a more advanced RegEx containing groups to easily allow dynamic id's
+            @commands.Cog.on_submit(custom_id='^ticket_answer:(?P<id>[0-9]+)$')
+            async def ticket_answer_callback(i: discord.ModalSubmitInteraction):
+                user_id = int(i.match['id'])
+                user = client.get_user(user_id) or await client.fetch_user(user_id)
+
 
         Raises
         ------
@@ -518,13 +532,13 @@ class Cog(metaclass=CogMeta):
             if not inspect.iscoroutinefunction(actual):
                 raise TypeError('event registered must be a coroutine function')
             actual.__cog_interaction_listener__ = True
-            _custom_id = re.compile(custom_id) if (
+            pattern = re.compile(custom_id) if (
                     custom_id is not None and not isinstance(custom_id, re.Pattern)
             ) else re.compile(f'^{actual.__name__}$')
             try:
-                actual.__interaction_listener_names__.append(('modal_submit', _custom_id))
+                actual.__interaction_listener_names__.append(('modal_submit', pattern))
             except AttributeError:
-                actual.__interaction_listener_names__ = [('modal_submit', _custom_id)]
+                actual.__interaction_listener_names__ = [('modal_submit', pattern)]
             return func
 
         return decorator
@@ -1233,7 +1247,7 @@ class Cog(metaclass=CogMeta):
                 bot.remove_listener(getattr(self, method_name))
 
             for (_type, method_name, custom_id) in self.__cog_interaction_listeners__:
-                bot.remove_interaction_listener(_type, getattr(self, method_name), custom_id)
+                bot.remove_interaction_listener(_type, getattr(self, method_name))
 
             bot.remove_application_cmds_from_cog(self)
 

commit ff4b7a87172b6d881f43740f431c33a995cbfe17

fix: Fixed TypeErrors' by on_click/select/submit in Cogs caused by 9f16b22
Signed-off-by: mccoderpy <mccuber04@outlook.de>

discord/ext/commands/bot.py
@@ -468,7 +468,7 @@ class BotBase(GroupMixin):
                 return False
 
         # We store this in the function here, so we can remove it later easily
-        listener = (func.__func__, _check)
+        listener = (func, _check)
         setattr(func.__func__, '__listener__', listener)
         listeners.append(listener)
 